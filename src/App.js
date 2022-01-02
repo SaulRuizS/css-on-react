0410c4ca7efa39d1be1681f4deef356b49f7d12b
Saul Ruiz <81715186+SaulRuizS@users.noreply.github.com>
2022-01-01 23:48:54 -0600

    Form and Title creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import "./styles.css";
+import React from "react";
+import Form from "./Form";
+import { Title } from "./Title";
+
+const movies = [
+  {
+    name: "Avengers",
+    available: 5
+  },
+  {
+    name: "Terminator",
+    available: 3
+  }
+];
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h2>Movies</h2>
+      {movies.map((movie) => (
+        <Form movie={movie} />
+      ))}
     </div>
   );
 }
